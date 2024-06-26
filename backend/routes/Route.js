391fd47a489ec4aee458123beb1094b176904578
Niyati2601 <niyatishah2601@gmail.com>
2024-06-26 16:45:33 +0530

    Login page and backend

--- backend/routes/Route.js
+++ backend/routes/Route.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const  signup  = require("../controllers/Signup");
+const  userLoginController  = require("../controllers/Login");
 
 router.post("/signup", signup);
+router.post("/login", userLoginController);
 
 module.exports = router
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Signup from "./pages/Signup";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
       <Toaster />
-      <Signup exact path="/signup" component={Signup} />
+      <Routes>
+      <Route exact path="/"  element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
   );
 }
