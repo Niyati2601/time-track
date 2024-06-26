9d54f4a9b7e7a657c4ccc4da04dd76727ffe488d
Niyati2601 <niyatishah2601@gmail.com>
2024-06-26 18:44:16 +0530

    added components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Signup from "./pages/Signup";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
       <Route exact path="/"  element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/home" element={<Home />} />
       </Routes>
     </>
   );
