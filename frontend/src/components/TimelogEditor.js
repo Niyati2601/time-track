501c1045cdb0577804853c5fa186ca165829ef45
Namrata-Chauhan8 <chauhannamrata856@gmail.com>
2024-07-02 18:21:02 +0530

    when user clockin then and then only time log will be open

--- frontend/src/components/TimelogEditor.js
+++ frontend/src/components/TimelogEditor.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { FaRegCirclePlay, FaRegCirclePause } from "react-icons/fa6";
 import { FiTag } from "react-icons/fi";
 import { MdOutlineTimer } from "react-icons/md";
@@ -8,6 +8,7 @@ import { IoClose } from "react-icons/io5";
 import toast from "react-hot-toast";
 import apiUrl from "../api/Api";
 import { useSelector } from "react-redux";
+import { ClockingContext } from "../context/ClockingContext";
 
 const TimelogEditor = ({ onOpen, onClose }) => {
   const [isPlay, setIsPlaying] = useState(false);
@@ -17,7 +18,7 @@ const TimelogEditor = ({ onOpen, onClose }) => {
   const [title, setTitle] = useState("");
 
   const user = useSelector((state) => state.user);
-  
+  const { isClocking, setIsClocking } = useContext(ClockingContext);
 
   const dropdownRef = useRef(null);
 
@@ -85,7 +86,8 @@ const TimelogEditor = ({ onOpen, onClose }) => {
   };
 
   return (
-    onOpen && (
+    onOpen &&
+    isClocking && (
       <div
         className="timelog-popup fixed bg-green-500 p-4 rounded-lg shadow-lg z-50 w-[400px] bottom-20 right-28"
         ref={dropdownRef}
