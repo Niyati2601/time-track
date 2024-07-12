9c96ddfb84dfd3ca37cead02761386daf8803f11
niyati <niyatis@zignuts.com>
2024-07-12 15:08:36 +0530

    merge changes

--- frontend/src/context/ClockingContext.js
+++ frontend/src/context/ClockingContext.js
@@ -4,11 +4,28 @@ import React, { createContext, useState, useEffect } from 'react';
 export const ClockingContext = createContext();
 
 export const ClockingProvider = ({ children }) => {
-  const [isClocking, setIsClocking] = useState(false);
-  const [clockInTime, setClockInTime] = useState();
-  const [clockOutTime, setClockOutTime] = useState();
-  const [isDayIn, setIsDayIn] = useState(false);
   const [logs, setLogs] = useState([]);
+  const [isClocking, setIsClocking] = useState(() => {
+    const savedIsClocking = localStorage.getItem('isClocking');
+    return savedIsClocking === 'true';
+  });
+  const [isDayIn, setIsDayIn] = useState(() => {
+    const savedIsDayIn = localStorage.getItem('isDayIn');
+    return savedIsDayIn === 'true';
+  });
+  const [clockInTime, setClockInTime] = useState(() => {
+    const savedClockInTime = localStorage.getItem('clockInTime');
+    return savedClockInTime ? new Date(savedClockInTime) : null;
+  });
+  const [clockOutTime, setClockOutTime] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem('isClocking', isClocking);
+    localStorage.setItem('isDayIn', isDayIn);
+    if (clockInTime) {
+      localStorage.setItem('clockInTime', clockInTime.toString());
+    }
+  }, [isClocking, isDayIn, clockInTime]);
 
 
   return (
